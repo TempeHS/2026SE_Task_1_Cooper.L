238c780fcfe20ef2bc4f2acb4eedff586f8bb08a
Cooper L <55725860+XCoop@users.noreply.github.com>
2025-04-08 04:08:56 +0000

    Commit

---
+++ my_work/main.py
@@ -0,0 +1,55 @@
+def main():
+    Choice_1 = input("Login - 1, Register - 2, Quit - 3 ")
+    if Choice_1 == "1":
+        Log_In()
+    elif Choice_1 == "2":
+        Register()
+    elif Choice_1 != "3":
+        main()
+
+def Register():
+    NewUser = input("Username? ")
+    with open("test.txt") as file:
+        for line in file:
+            row = line.rstrip().split(",")  
+            if NewUser == row[0]:
+                print("Username Already Taken")
+                Register()
+    NewPass = input("Password? ")
+    with open("test.txt", "a") as file:
+        file.write(f"{NewUser},{NewPass}\n")
+        main()
+
+
+def Logged_In():
+    Choice_2 = input("Change Password - 1, Logout - 2 ")
+    if Choice_2 == "1":
+        NewPassword = (input("New Password? "))
+    elif Choice_2 == "2":
+        main()
+    else:
+        Logged_In()
+
+
+
+
+def Log_In():
+    Username = input("Username? ")
+    Password = input("Password? ")
+    data1 = 0
+    with open("test.txt") as file:
+        for line in file:
+            row = line.rstrip().split(",")  
+            if Username + Password == row[0] + row[1]:
+                data1 = 1
+                Logged_In()
+        if data1 == 0:
+            Log_In()
+
+
+
+
+
+
+
+main()
